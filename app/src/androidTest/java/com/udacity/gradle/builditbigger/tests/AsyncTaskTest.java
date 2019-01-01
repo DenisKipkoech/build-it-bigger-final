b50d137994ec857ecc890c560848cda8853b67e7
Denis <d3niskip@gmail.com>
2019-01-01 08:40:31 +0300

    feat: added paid flavor

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/tests/AsyncTaskTest.java
@@ -0,0 +1,22 @@
+package com.udacity.gradle.builditbigger.tests;
+
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.udacity.gradle.builditbigger.EndpointAsync;
+import com.udacity.gradle.builditbigger.MainActivity;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by denis on 01/01/19.
+ */
+@RunWith(AndroidJUnit4.class)
+public class AsyncTaskTest {
+    @Test
+    public void jokeNotNull() throws Exception{
+
+    }
+}
---
+++ app/src/main/java/MainActivityFragment.java
@@ -0,0 +1,23 @@
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.udacity.gradle.builditbigger.R;
+
+/**
+ * Created by denis on 01/01/19.
+ */
+
+public class MainActivityFragment extends Fragment {
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return root;
+    }
+}
